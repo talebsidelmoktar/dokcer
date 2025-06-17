60396b047dfdd78cca04f37dffe6e20d0a13c461
talebsidelmoktar <93160372+talebsidelmoktar@users.noreply.github.com>
2025-06-17 12:09:31 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -26,23 +26,15 @@ define('DB_HOST', $db_host . ':' . $db_port);
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
-// Test database connection
-try {
-    $test_connection = new PDO("pgsql:host=$db_host;port=$db_port;dbname=$db_name", $db_user, $db_password);
-    $test_connection = null; // Close connection
-} catch (PDOException $e) {
-    die('Database connection failed: ' . $e->getMessage());
-}
-
-// Security keys - Generate new ones from https://api.wordpress.org/secret-key/1.1/salt/
-define('AUTH_KEY',         ':g3r*PTd$G,%w5sK9e2_s#8u(|>Ia?,s6Thb#r(v-=ny@4@Wvm+>c7RL@GS/#66#');
-define('SECURE_AUTH_KEY',  '-+%^g38D6tc9+&]n8DQ+zQDuOVIHj,1#q]<BvxpqqvReTXVD6gC)Q:m>EtK;Y<-4');
-define('LOGGED_IN_KEY',    'qACJ*3a4J9vAF-(x-}(E2!l,>`o:kqu)srtE^B3uCmI%vap9&h@;OH4:CRiY[`It');
-define('NONCE_KEY',        'QU$WSl<AS-?{Dn+lXyf5f.<0/KTPav 2:(`Kp(P&fMIbKcT@I(6nect_fqgfJ[a2');
-define('AUTH_SALT',        'Mi~A?G$&+B!-u$^6CM+%zN-Q)AO}<FFm*|($@vP|$~sw)p+C,YS#[M5ytZtg1gfD');
-define('SECURE_AUTH_SALT', 'K &D%w|cJMQ)7_5;L7.4Pzb-c6bzq`P|,T>6v{VniaMT5suZNQQ-WnEaE?4qAct-');
-define('LOGGED_IN_SALT',   '6f-Dw#K@)z2c2_H=t[BB!qs^^FtE|$3@5yRG|Mp|AI+}yGGBPY0F^u,-qDmIu|_q');
-define('NONCE_SALT',       'B.pehEBtPC~~hif-0u.+BQMME+M`]`Iv3iqV0%IzWb2PI /zfDK;uX*X/{t|9{!+');
+// Security keys - REPLACE THESE with keys from https://api.wordpress.org/secret-key/1.1/salt/
+define('AUTH_KEY',         '&^awm2%J3t/z6q-M^2 ny!n3yc_WYTf O}F^DW,c --gf5!Bp./HHo5LWUl*/G-d');
+define('SECURE_AUTH_KEY',  'My>nRqO&iuX0%3:qYP3^Rn1yo@<JV~yeN90~7,u+nU#,:_e-a=hNvacWrHL/fRl$');
+define('LOGGED_IN_KEY',    '9o#(T a-zdO;~$eV -LPG&#tTXVIr40^_&4>ds3 7SmE&A!C71yFEsrANg^iz0eF');
+define('NONCE_KEY',        'Cd15</k-&yEBy^S&BH%8^_&U{B/^?=Zz_9Ma&-Y_qTw4/&,ckKi@])Urq{}2BM~<');
+define('AUTH_SALT',        '<>4@*_MX;uE}-Oc$QsTpj5.Y,lWgN;3R-CbH?^U39gaLYfSj.^7DVXZ{X$@CYr1I');
+define('SECURE_AUTH_SALT', 'w.&,qO.KT 8+.=S.oCqX-osXpu*,CaUP+U@4CL-[zX`B<./i80GpN^E:d>RJDpF0');
+define('LOGGED_IN_SALT',   'g>8]t`K4p%*4LdAm+Dn,7}5w5Lj^Sr|nVDA3?@]VKy|f[=zJ>Zv:?RJ,0<c+A1Tm');
+define('NONCE_SALT',       'q[XKcojl$RVqc)whTBm2-7E2d;2d g<4m%i1At1^_(vI?KRtV!w-zI(/@Z6|U7I=');
 
 // Table prefix
 $table_prefix = getenv('WORDPRESS_TABLE_PREFIX') ?: 'wp_';
@@ -51,7 +43,6 @@ $table_prefix = getenv('WORDPRESS_TABLE_PREFIX') ?: 'wp_';
 define('WP_DEBUG', true);
 define('WP_DEBUG_LOG', true);
 define('WP_DEBUG_DISPLAY', false);
-define('SCRIPT_DEBUG', true);
 
 // Force HTTPS on Render
 if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
@@ -59,7 +50,7 @@ if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROT
     $_SERVER['SERVER_PORT'] = 443;
 }
 
-// WordPress URLs - Let WordPress auto-detect
+// WordPress URLs
 if (isset($_SERVER['HTTP_HOST'])) {
     $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || $_SERVER['SERVER_PORT'] == 443 ? 'https://' : 'http://';
     define('WP_HOME', $protocol . $_SERVER['HTTP_HOST']);
@@ -71,9 +62,6 @@ define('WP_MEMORY_LIMIT', '256M');
 
 // File permissions
 define('FS_METHOD', 'direct');
-
-// Disable file editing
-define('DISALLOW_FILE_EDIT', true);
 
 // Absolute path
 if (!defined('ABSPATH')) {
